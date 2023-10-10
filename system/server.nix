8c902fb7b610798dcd0062f3084451af115b6862
vamoosev <vamoosev@gmail.com>
2023-10-10 13:46:40 +0300

    vausdasd

--- system/server.nix
+++ system/server.nix
@@ -137,4 +137,18 @@
       email="mestarit@ksyk.fi";
     };
 
+   services.roundcube = {
+     enable = true;
+     # this is the url of the vhost, not necessarily the same as the fqdn of
+     # the mailserver
+     hostName = "webmail.itmestarit.fi";
+     extraConfig = ''
+       # starttls needed for authentication, so the fqdn required to match
+       # the certificate
+       $config['smtp_server'] = "tls://${config.mailserver.fqdn}";
+       $config['smtp_user'] = "%u";
+       $config['smtp_pass'] = "%p";
+     '';
+  };
+
 }
